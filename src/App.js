6f0e7c1e9292ca7077317b9e1be3bab126645b5b
AkhilAkhill <99791968+AkhilAkhill@users.noreply.github.com>
2023-02-14 16:31:21 +0530

    Add files via upload
    nnnnnnnn

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import'./App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <select className='school'>
+        <option>Classes</option>
+          <option>LKG</option>
+          <option>UKG</option>
+          <option>1st</option>
+          <option>2nd</option>
+          <option>3rd</option>
+          <option>4th</option>
+          <option>5th</option>
+          <option>6th</option>
+          <option>7th</option>
+          <option>8th</option>
+          <option>9th</option>
+          <option>10th</option>
+        </select>
+        </div>
+    
+    </div>
+  )
+}
+
+export default App
